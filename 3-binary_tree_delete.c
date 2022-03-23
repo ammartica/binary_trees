7114784b28e566831f5c602090d294d0fc2cd191
ammartica <amisadaymartinez@gmail.com>
2022-03-23 17:57:57 +0000

    removed unecessary else statement

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -10,10 +10,7 @@ void binary_tree_delete(binary_tree_t *tree)
 {
 	if (!tree)
 		return;
-	else
-	{
-		binary_tree_delete(tree->right);
-		binary_tree_delete(tree->left);
-		free(tree);
-	}
+	binary_tree_delete(tree->right);
+	binary_tree_delete(tree->left);
+	free(tree);
 }
